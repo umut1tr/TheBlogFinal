038ffa3e9de46c7cdd2bbf5b6d50ece3dc50b126
Umut <55415203+umut1tr@users.noreply.github.com>
2021-10-21 21:04:08 +0200

    updated POSTGRE Connection

--- Startup.cs
+++ Startup.cs
@@ -28,9 +28,14 @@ namespace TheBlogFinal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(
+            //        Configuration.GetConnectionString("DefaultConnection")));
+
+            // custom PostGRE connect
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                  options.UseNpgsql(
+        Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
