77b5d8f2d72beadc029cf12677cde9947fca7d83
Umut <55415203+umut1tr@users.noreply.github.com>
2021-10-27 19:32:21 +0200

    Updated MailSettings.

---
+++ ViewModels/MailSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBlogFinal.ViewModels
+{
+    public class MailSettings
+    {
+
+        //So we can configure and use and smtp server
+        //from hetzner for example
+
+        public string Mail { get; set; }
+        public string DisplayName { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+    }
+}
